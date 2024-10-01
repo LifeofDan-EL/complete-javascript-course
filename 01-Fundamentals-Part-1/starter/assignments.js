f81f96ddf4ccefcb3bc3a65f9384ce7ace5a1539
Daniel Anomfueme <anomfuemedaniel@gmail.com>
2024-10-01 19:14:38 +0100

    completed assignment

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,8 @@
+let country = "Nigeria";
+let continent = "Africa";
+let population = "232,679,478";
+
+
+console.log(country);
+console.log(continent);
+console.log(population);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,19 @@
+
+let js = 'amadzing';
+
+console.log( 40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Daniel";
+console.log (firstName);
+console.log (firstName);
+console.log (firstName);
+console.log (firstName);
+
+let myFirstJob = "Programmer";
+let myCurrebtJob = "Teacher";
+
+console.log(myFirstJob);
+console.log(myCurrebtJob);
