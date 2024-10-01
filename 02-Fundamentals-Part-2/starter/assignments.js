db70a30a3fa5ed1c29a048369f7f2fbc1ee13114
Daniel Anomfueme <anomfuemedaniel@gmail.com>
2024-10-01 22:07:03 +0100

    Completed assignment

---
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -0,0 +1,22 @@
+
+/*
+LECTURE: Data Types
+1. Declare a variable called 'isIsland' and set its value according to your 
+country. The variable should hold a Boolean value. Also declare a variable
+'language', but don't assign it any value yet
+2. Log the types of 'isIsland', 'population', 'country' and 'language'
+to the console
+*/
+
+let country = "Nigeria";
+let continent = "Africa";
+let population = "232,679,478";
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof continent);
+console.log(typeof language)
+
