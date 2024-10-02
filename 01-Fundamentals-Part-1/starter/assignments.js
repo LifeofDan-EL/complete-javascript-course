e30e43507260399398c7792284e49a6aeb8ac59f
Daniel Anomfueme <anomfuemedaniel@gmail.com>
2024-10-02 23:22:13 +0100

    Completed basic operator assignment

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -5,11 +5,35 @@ assign their values according to your own country (population in millions)
 2. Log their values to the console
 */
 
-let country = "Nigeria";
-let continent = "Africa";
-let population = "232,679,478";
-
+const country = "Nigeria";
+const continent = "Africa";
+let nigeriaPopulation = 232679478;
+const isIsland = false;
+let language = "English";
+const finlandPopulation = 6000000;
+const averagePopulation = 33000000;
 
 console.log(country);
 console.log(continent);
-console.log(population);
+console.log(nigeriaPopulation);
+
+console.log(typeof isIsland);
+console.log(typeof nigeriaPopulation);
+console.log(typeof country);
+console.log(typeof continent);
+console.log(typeof language);
+
+console.log(nigeriaPopulation / 2);
+nigeriaPopulation++;
+console.log(nigeriaPopulation);
+console.log(nigeriaPopulation > finlandPopulation);
+console.log(nigeriaPopulation < averagePopulation);
+console.log(
+  country +
+    " is in " +
+    continent +
+    ", and its " +
+    nigeriaPopulation +
+    " people speak " +
+    language
+);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,4 @@
-
+/*
 let js = 'amadzing';
 
 console.log( 40 + 8 + 23 - 10);
@@ -17,3 +17,66 @@ let myCurrebtJob = "Teacher";
 
 console.log(myFirstJob);
 console.log(myCurrebtJob);
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Jonas");
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
+
+
+let age = 24;
+console.log(age)
+age = 25
+console.log(age)
+
+const birthYear = 2000;
+console.log(birthYear);
+birthYear = 2001;
+console.log(age);
+*/
+
+const now = 2037;
+const ageDaniel = now - 2000;
+const ageSarah = now - 1997;
+console.log(ageDaniel, ageSarah);
+
+console.log(ageDaniel * 2, ageDaniel / 10, 11 ** 3);
+
+const firstName = "Daniel";
+const lastName = "Anomfueme";
+
+console.log(firstName + " " + lastName);
+
+//Assignmenmt Operators
+let x = 10 + 5; //15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--;
+x--;
+console.log(x);
+
+// Comparison operators
+console.log(ageDaniel > ageSarah);
+console.log(ageDaniel >= 37);
+
+const isFullAge = ageDaniel >= 37;
+
+console.log(isFullAge);
