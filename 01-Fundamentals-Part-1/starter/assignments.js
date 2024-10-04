14d9e3db367ba6d9e704a5ea559597e3224742a9
Daniel Anomfueme <anomfuemedaniel@gmail.com>
2024-10-04 17:00:56 +0100

    Completed coding challenge #4

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -6,7 +6,7 @@ const country = "Nigeria";
 const continent = "Africa";
 let nigeriaPopulation = 232679478;
 const isIsland = false;
-let language = "English";
+let language = "english";
 const finlandPopulation = 6000000;
 const averagePopulation = 33000000;
 let description;
@@ -48,11 +48,37 @@ console.log(
   5 + 6 + "4" + 9 - 4 - 2
 );
 
-const numNeighbours =
-  Number();
-  // prompt("How many neighbour countries does your country have?")
+const numNeighbours = Number();
+// prompt("How many neighbour countries does your country have?")
 if (numNeighbours === 1) {
   console.log("Only 1 border!");
 } else if (numNeighbours > 1) {
   console.log("More than 1 border");
 } else console.log("No borders");
+
+switch (language) {
+  case "chinese":
+  case "mandarin":
+    console.log("MOST number of native speakers!");
+    break;
+  case "spanish":
+    console.log("2nd place in number of native speakers");
+    break;
+  case "english":
+    console.log("3rd place");
+    break;
+  case "hindi":
+    console.log("Number 4");
+    break;
+  case "arabic":
+    console.log("5th most spoken language");
+    break;
+  default:
+    console.log("Great language too :D");
+}
+
+console.log(
+  `${country}'s population is ${
+    nigeriaPopulation > 3300000 ? "above" : "below"
+  } average`
+);
